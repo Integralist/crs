06e69f1b586ec12eceb8d75dc3e1c21b94f274cb
Mark McDonnell <mark.mcdx@gmail.com>
2022-03-19 20:17:27 +0000

    basic flag parsing with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use clap::{ArgGroup, Parser};
+
+const ABOUT: &str = "A tool that issues HTTP requests, then parses, sorts and displays relevant HTTP response headers.";
+
+#[derive(Parser, Debug)]
+#[clap(author, version = "1.0.0", about = ABOUT)]
+#[clap(group(
+        ArgGroup::new("format")
+        .args(&["json", "plain"]),
+))]
+struct Args {
+    /// Comma-separated list of headers to be displayed
+    #[clap(short, long, required = false)]
+    filter: String,
+    /// Output is formatted into JSON for easy parsing
+    #[clap(short, long)]
+    json: bool,
+    /// Output is formatted without any extraneous spacing or ANSI colour code
+    #[clap(short, long)]
+    plain: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("{:?}", args)
 }
