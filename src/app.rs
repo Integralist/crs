5db92a6c28185b8648be6e838f17cbe8eb8a981c
Mark McDonnell <mark.mcdx@gmail.com>
2022-03-20 20:49:01 +0000

    create app module to hold arg parser logic

---
+++ src/app.rs
@@ -0,0 +1,56 @@
+use clap::{ArgEnum, ArgGroup, Parser};
+use owo_colors::{colors::*, OwoColorize, Stream::Stdout};
+
+const ABOUT: &str = "A tool that issues HTTP requests, then parses, sorts and displays relevant HTTP response headers.";
+
+#[derive(Parser, Debug)]
+#[clap(author, version = "1.0.0", about = ABOUT)]
+#[clap(group(
+        ArgGroup::new("format")
+        .args(&["json", "color"]),
+))]
+pub struct App {
+    /// Output formatting can be modified based on TTY
+    #[clap(short, long, arg_enum, default_value = "auto")]
+    color: Color,
+    /// Comma-separated list of headers to display
+    #[clap(short, long)]
+    filter: Option<String>,
+    /// Output is formatted into JSON
+    #[clap(short, long)]
+    json: bool,
+    /// Output displays additional contextual information
+    #[clap(short, long, global = true)]
+    verbose: bool,
+}
+
+impl App {
+    pub fn exec(self) -> Result<(), Box<dyn std::error::Error>> {
+        self.color.init();
+
+        println!(
+            "{} {:?}",
+            "foo".if_supports_color(Stdout, |text| text.bg::<BrightYellow>().fg::<Black>()),
+            self
+        );
+
+        Ok(())
+    }
+}
+
+#[derive(ArgEnum, Clone, Copy, Debug)]
+enum Color {
+    Always,
+    Auto,
+    Never,
+}
+
+impl Color {
+    fn init(self) {
+        match self {
+            Color::Always => owo_colors::set_override(true),
+            Color::Auto => {}
+            Color::Never => owo_colors::set_override(false),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,9 @@
-use clap::{ArgEnum, ArgGroup, Parser};
-use owo_colors::{colors::*, OwoColorize, Stream::Stdout};
+mod app;
 
-const ABOUT: &str = "A tool that issues HTTP requests, then parses, sorts and displays relevant HTTP response headers.";
+use crate::app::App;
+use clap::Parser;
 
-#[derive(Parser, Debug)]
-#[clap(author, version = "1.0.0", about = ABOUT)]
-#[clap(group(
-        ArgGroup::new("format")
-        .args(&["json", "color"]),
-))]
-struct Args {
-    /// Output formatting can be modified based on TTY
-    #[clap(short, long, arg_enum, default_value = "auto")]
-    color: Color,
-    /// Comma-separated list of headers to display
-    #[clap(short, long)]
-    filter: Option<String>,
-    /// Output is formatted into JSON
-    #[clap(short, long)]
-    json: bool,
-    /// Output displays additional contextual information
-    #[clap(short, long, global = true)]
-    verbose: bool,
-}
-
-#[derive(ArgEnum, Clone, Copy, Debug)]
-enum Color {
-    Always,
-    Auto,
-    Never,
-}
-
-impl Color {
-    fn init(self) {
-        match self {
-            Color::Always => owo_colors::set_override(true),
-            Color::Auto => {}
-            Color::Never => owo_colors::set_override(false),
-        }
-    }
-}
-
-fn main() {
-    let args = Args::parse();
-    args.color.init();
-
-    println!(
-        "{} {:?}",
-        "foo".if_supports_color(Stdout, |text| text.bg::<BrightYellow>().fg::<Black>()),
-        args
-    )
+fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
+    let app = App::parse();
+    app.exec()
 }
