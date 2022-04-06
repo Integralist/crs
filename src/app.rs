62d4e9c21204c5d0b8952d5418d9b8a60e42d579
Mark McDonnell <mark.mcdx@gmail.com>
2022-04-06 16:18:06 +0100

    refactor the primary logic out of the args module

--- src/app.rs
+++ src/app.rs
@@ -1,15 +1,16 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use clap::Parser;
 use crate::args::Args;
+use crate::headers::Headers;
 use std::ffi::OsString;
 
+/// run parses the given itr arguments and triggers the primary program logic.
 pub fn run<I, T>(itr: I) -> Result<()>
 where
     I: IntoIterator<Item = T>,
     T: Into<OsString> + Clone,
 {
-    let args = Args::parse_from(itr);
-    args.exec()
+    exec(Args::parse_from(itr))
 }
 
 #[test]
@@ -21,4 +22,17 @@ fn run_success() {
         "https://www.fastly.com".to_string(),
     ];
     run(itr).expect("to run correctly");
+}
+
+/// exec makes a HTTP request for the configured URL and constructs a Header for display.
+fn exec(args: Args) -> Result<()> {
+    args.color.init();
+
+    let resp = reqwest::blocking::get(&args.url)
+        .with_context(|| format!("Failed to GET: {}", &args.url))?;
+
+    let headers = Headers::new(resp.headers(), &args.filter);
+    headers.parse()?.display(args.json, resp.status());
+
+    Ok(())
 }
--- src/args.rs
+++ src/args.rs
@@ -1,6 +1,4 @@
-use anyhow::{Context, Result};
 use clap::{ArgGroup, Parser};
-use crate::headers::Headers;
 use crate::styles::Color;
 
 const ABOUT: &str =
@@ -15,27 +13,13 @@ const ABOUT: &str =
 pub struct Args {
     /// Output formatting can be modified based on TTY
     #[clap(short, long, arg_enum, default_value = "auto")]
-    color: Color,
+    pub color: Color,
     /// Comma-separated list of headers to display
     #[clap(short, long)]
-    filter: Option<String>,
+    pub filter: Option<String>,
     /// Output is formatted into JSON
     #[clap(short, long)]
-    json: bool,
+    pub json: bool,
     /// URL to request
-    url: String,
-}
-
-impl Args {
-    pub fn exec(self) -> Result<()> {
-        self.color.init();
-
-        let resp = reqwest::blocking::get(&self.url)
-            .with_context(|| format!("Failed to GET: {}", &self.url))?;
-
-        let headers = Headers::new(resp.headers(), &self.filter);
-        headers.parse()?.display(self.json, resp.status());
-
-        Ok(())
-    }
+    pub url: String,
 }
